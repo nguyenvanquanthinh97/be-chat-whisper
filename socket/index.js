dca52a6666bae2dbaac6c9d1f92c03359b053713
Thinh97 <nvq.thinh.hcmus.220597@gmail.com>
2020-03-31 13:09:51 +0700

    Add catch error

--- socket/index.js
+++ socket/index.js
@@ -11,42 +11,46 @@ const Room = require('../model/room');
 const UserActivity = require('../model/user-activity');
 
 module.exports.mainSocket = async (io) => {
-  const companies = await Company.find({}, { _id: 1 });
-  const companyIds = companies.map(company => get(company, '_id'));
-  companyIds.map((companyId) => {
-    io.of(`/${companyId}`).use(async (socket, next) => {
-      const token = get(socket, 'handshake.query.token').split(' ')[1];
-      let decoded;
-      if (!token) {
-        return next(new Error("Token doesn't exist"));
-      }
+  try {
+    const companies = await Company.find({}, { _id: 1 });
+    const companyIds = companies.map(company => get(company, '_id'));
+    companyIds.map((companyId) => {
+      io.of(`/${companyId}`).use(async (socket, next) => {
+        const token = get(socket, 'handshake.query.token').split(' ')[1];
+        let decoded;
+        if (!token) {
+          return next(new Error("Token doesn't exist"));
+        }
 
-      const blackJWT = await BlackList.findOne({ jwt: token });
+        const blackJWT = await BlackList.findOne({ jwt: token });
 
-      if (blackJWT) {
-        return next(new Error("Invalid Token"));
-      }
+        if (blackJWT) {
+          return next(new Error("Invalid Token"));
+        }
 
-      try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET);
-      } catch (err) {
-        return next(new Error(err));
-      }
+        try {
+          decoded = jwt.verify(token, process.env.JWT_SECRET);
+        } catch (err) {
+          return next(new Error(err));
+        }
 
-      socket.companyId = get(decoded, 'companyId');
-      socket.userId = get(decoded, 'userId');
-      socket.username = get(decoded, 'username');
-      socket.email = get(decoded, 'email');
-      return next();
-    }).on('connect', async (socket) => {
-      console.log(socket.id + " has connected to Server");
-  
-      const companyId = get(socket, 'companyId');
-      const userId = get(socket, 'userId');
-      const username = get(socket, 'username');
-      const email = get(socket, 'email');
-  
-      socket.emit("roomList", await fetchRooms(companyId, userId, email, username));
+        socket.companyId = get(decoded, 'companyId');
+        socket.userId = get(decoded, 'userId');
+        socket.username = get(decoded, 'username');
+        socket.email = get(decoded, 'email');
+        return next();
+      }).on('connect', async (socket) => {
+        console.log(socket.id + " has connected to Server");
+
+        const companyId = get(socket, 'companyId');
+        const userId = get(socket, 'userId');
+        const username = get(socket, 'username');
+        const email = get(socket, 'email');
+
+        socket.emit("roomList", await fetchRooms(companyId, userId, email, username));
+      });
     });
-  })
+  } catch (error) {
+    socket.on('error', error);
+  }
 };
